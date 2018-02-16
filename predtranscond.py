3184acf68d69c8dd51e7587d91c0fa6e65bd204d
= <=>
2018-02-15 16:42:11 -0800

    changed some comments

--- predtranscond.py
+++ predtranscond.py
@@ -1,8 +1,3 @@
-#with this submission, reached 80/896 on private leaderboard on kaggle (top 10%), but was 13 minutes past the deadline...
-#the private score (remaining 83% of their test data) was 0.0643 root mean square error, top scorer reached ~0.05
-
-#code to generate xyz data features was borrowed from Tony Y. https://www.kaggle.com/tonyyy/how-to-get-atomic-coordinates 
-#and PCA for xyz data was borrowed from DatumX https://www.kaggle.com/janpreets/using-the-atomic-coordinates-for-prediction
 import numpy as np
 import pandas as pd
 import xgboost as xgb
